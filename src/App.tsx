d076962e55692e671f911a37e4560912a2954f53
bechir mahamat <bechirmahamat11@gmail.com>
2024-02-26 21:29:34 +0300

    fixed bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,56 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-
+import {
+    Route,
+    RouterProvider,
+    Routes,
+    createBrowserRouter,
+} from 'react-router-dom'
 import LoginForm from './_auth/forms/LoginForm'
 import RegisterForm from './_auth/forms/RegisterForm'
+import Error from './_root/Error'
 import { Home, RootLayout } from './_root/pages'
-
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <RootLayout />,
-        errorElement: <Error />,
-        children: [
-            {
-                index: true,
-            },
-        ],
-    },
-    {
-        path: '/login',
-        element: <LoginForm />,
-    },
-    {
-        path: '/register',
-        element: <RegisterForm />,
-    },
-])
+import AuthLayout from './_auth/AuthLayout'
+// const router = createBrowserRouter([
+//     {
+//         path: '/',
+//         element: <RootLayout />,
+//         errorElement: <Error />,
+//         children: [
+//             {
+//                 index: true,
+//             },
+//         ],
+//     },
+//     {
+//         path: '/auth',
+//         element: <AuthLayout />,
+//         children: [
+//             {
+//                 path: '/login',
+//                 element: <LoginForm />,
+//             },
+//             {
+//                 path: '/register',
+//                 element: <RegisterForm />,
+//             },
+//         ],
+//     },
+// ])
 function App() {
-    return <RouterProvider router={router}></RouterProvider>
+    return (
+        <>
+            <Routes>
+                {/* private */}
+                <Route element={<RootLayout />}>
+                    <Route path='/' element={<Home />} />
+                </Route>
+                {/* public */}
+                <Route element={<AuthLayout />}>
+                    <Route path='/login' element={<LoginForm />} />
+                    <Route path='/register' element={<RegisterForm />} />
+                </Route>
+            </Routes>
+        </>
+    )
 }
 
 export default App
